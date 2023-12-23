4e0bb6e4a831bbd4b3e360dce28f7254779e1264
Shin-sibainu <shincode0712@gmail.com>
2023-12-23 13:15:11 +0900

    mod

---
+++ src/types/Types.ts
@@ -0,0 +1,13 @@
+// type Post = {
+//   title: string;
+//   author: string;
+//   description: string;
+//   image: {
+//     url: string;
+//     alt: string;
+//   };
+//   pubData: Date;
+//   tags: string[];
+// };
+
+// export type { Post };
